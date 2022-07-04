6a0ed1f52b1d95f84f07cca7f9bd870ce9ef2ce4
SockSock <anish.tron@gmail.com>
2022-07-04 15:35:22 +0100

    added comments

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,17 +1,20 @@
+// Main class. This is the main entry point where the game starts when it is executed.
+
 import {Board} from './board.js';
 import {Pacman} from './pacman.js';
 
 let pacman;
 let board;
 
+// Loads and defines initial properties which are needed before the game starts.
 window.setup = function() {
     rectMode(CENTER);
     createCanvas(600, 750);
-    keyCode = 65;
     board = new Board();
     pacman = new Pacman();
 }
 
+// Updates 60 frames per second to reload objects to update their locations.
 window.draw = function() {
     scale(3);
     background(0);
@@ -21,6 +24,7 @@ window.draw = function() {
     pacman.stopSprite();
 }
 
+// Detects keys inputted by the user to allow for movement of Pac-Man.
 window.keyPressed = function() {
     pacman.changeDirection();
 }
